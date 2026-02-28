561a9ea44a2dd6a78c62910e86ba8818435404c6
JCallicoat7 <pokemonman873@gmail.com>
2026-02-27 20:34:34 -0500

    Added Feeder, Hanger, Landmarks, and Ports

---
+++ src/main/java/frc/robot/Landmarks.java
@@ -0,0 +1,23 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import static edu.wpi.first.units.Units.Inches;
+
+import java.util.Optional;
+
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+
+public class Landmarks {
+    public static Translation2d hubPosition() {
+        final Optional<Alliance> alliance = DriverStation.getAlliance();
+        if (alliance.isPresent() && alliance.get() == Alliance.Blue) {
+            return new Translation2d(Inches.of(182.105), Inches.of(158.845));
+        }
+        return new Translation2d(Inches.of(469.115), Inches.of(158.845));
+    }
+}
